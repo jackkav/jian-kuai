0bb215f6930e71061e33d574514f411877bbe3fe
Jack Kavanagh <jackkav@gmail.com>
2017-04-25 23:22:53 +0800

    letter A

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.tile}>
+          <Text style={styles.letter}>A</Text>
+        </View>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#644B62'
   },
-});
+  tile: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#BEE1D2'
+  },
+  letter: {
+    color: '#333',
+    fontSize: 80
+  }
+})
