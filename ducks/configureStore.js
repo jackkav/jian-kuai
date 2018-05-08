cd08268ef154031fc383b5ac4244c1f547f87289
Jack Kavanagh <jackkav@gmail.com>
2018-05-08 18:06:15 +0800

    expo 16=>27

--- ducks/configureStore.js
+++ ducks/configureStore.js
@@ -1,16 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'remote-redux-devtools'
 import app from './reducers'
 
 import { createEpicMiddleware } from 'redux-observable'
 import fetchUserEpic from './epic'
 
-const epicMiddleware = createEpicMiddleware(fetchUserEpic)
 
 export default function configureStore () {
   const store = createStore(
     app,
-    composeWithDevTools(applyMiddleware(epicMiddleware))
+    applyMiddleware(createEpicMiddleware(fetchUserEpic))
   )
   return store
 }
