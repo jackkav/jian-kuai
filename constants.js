0f6f4175fe3351ff8bd1426e6bdb476c0d5fd312
Jack Kavanagh <jackkav@gmail.com>
2020-04-26 12:47:23 +0200

    upgrade 37

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
-var { width, height } = require('Dimensions').get('window')
+import {Dimensions} from 'react-native'
+var { width, height } = Dimensions.get('window')
 export const SIZE = 4 // four-by-four grid
 export const CELL_SIZE = Math.floor(width * 0.2) // 20% of the screen width
 export const CELL_PADDING = Math.floor(CELL_SIZE * 0.05) // 5% of the cell size
